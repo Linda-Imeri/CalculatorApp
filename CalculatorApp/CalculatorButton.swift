a6f2de8f31212f6de0fca640a39af3e0cac5d924
Linda-Imeri <lindaimmeri@gmail.com>
2022-10-25 18:44:05 +0200

    Add CalculatorButton UI,  add circle and text for button label

---
+++ CalculatorApp/CalculatorButton.swift
@@ -0,0 +1,35 @@
+//
+//  CalculatorButton.swift
+//  CalculatorApp
+//
+//  Created by Linda Imeri on 25.10.22.
+//
+
+import SwiftUI
+
+struct CalculatorButton: View {
+    var label: String
+    var color: Color
+    
+    var body: some View {
+        Button {
+            //Inform model of button press
+            
+        } label: {
+            ZStack{
+                Circle().fill(color)
+                Text(label).font(.title)
+            }
+        }
+        //White color for button text
+        .accentColor(.white)
+
+    }
+}
+
+struct CalculatorButton_Previews: PreviewProvider {
+    static var previews: some View {
+        CalculatorButton(label: "1", color:.gray)
+            .previewLayout(.fixed(width: 100, height: 100))
+    }
+}
