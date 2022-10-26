73392d7d5fd4f76c2c4f6e0a330c762b4ddd6461
Linda-Imeri <lindaimmeri@gmail.com>
2022-10-26 12:17:34 +0200

    Add custom Colors, Display the rows of buttons each with specified labels

--- CalculatorApp/CalculatorHome.swift
+++ CalculatorApp/CalculatorHome.swift
@@ -6,11 +6,33 @@
 //
 
 import SwiftUI
+//globl constants
+//Custom colors
+let darkerGray = Color(CGColor(gray: 0.1, alpha: 1))
+let darkGray = Color(CGColor(gray: 0.3, alpha: 1))
 
 struct CalculatorHome: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .trailing, spacing: 0){
+            Spacer()
+            //Display the current value
+            Text("2")
+                .foregroundColor(.white)
+                .font(.system(size: 40))
+                .lineLimit(1)
+                .padding(.trailing,20)
+            
+            //Display the rows of buttons, each with specified labels
+            VStack(spacing: 10){
+                CalculatorRow(labels: ["CE","","",String("\u{00f7}")], colors: [darkGray,darkGray,darkGray,.orange])
+                CalculatorRow(labels: ["7","8","9",String("\u{00f7}")])
+                CalculatorRow(labels: ["4","5","6","-"])
+                CalculatorRow(labels: ["1","2","3","+"])
+                CalculatorRow(labels: ["0",".","","="])
+            }.padding()
+        }
+        .background(darkerGray)
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
