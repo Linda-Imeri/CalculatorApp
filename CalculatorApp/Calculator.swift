6ed1cf29da56da95cab86b9cf73bdf646de66b79
Linda-Imeri <lindaimmeri@gmail.com>
2022-10-26 13:14:24 +0200

    Create Calculator class and Operation class

---
+++ CalculatorApp/Calculator.swift
@@ -0,0 +1,12 @@
+//
+//  Calculator.swift
+//  CalculatorApp
+//
+//  Created by Linda Imeri on 26.10.22.
+//
+
+import Foundation
+
+class Calculator: ObservableObject{
+    
+}
---
+++ CalculatorApp/Operator.swift
@@ -0,0 +1,12 @@
+//
+//  Operator.swift
+//  CalculatorApp
+//
+//  Created by Linda Imeri on 26.10.22.
+//
+
+import Foundation
+
+class Operator {
+    
+}
