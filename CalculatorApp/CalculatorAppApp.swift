c2eeadbbfc3cc0f941f0378f5185681fc867ff57
Linda-Imeri <lindaimmeri@gmail.com>
2022-10-26 14:06:03 +0200

    Create environmentObject

--- CalculatorApp/CalculatorAppApp.swift
+++ CalculatorApp/CalculatorAppApp.swift
@@ -12,6 +12,7 @@ struct CalculatorAppApp: App {
     var body: some Scene {
         WindowGroup {
             CalculatorHome()
+                .environmentObject(Calculator())
         }
     }
 }
--- CalculatorApp/CalculatorHome.swift
+++ CalculatorApp/CalculatorHome.swift
@@ -12,12 +12,13 @@ let darkerGray = Color(CGColor(gray: 0.1, alpha: 1))
 let darkGray = Color(CGColor(gray: 0.3, alpha: 1))
 
 struct CalculatorHome: View {
+    @EnvironmentObject var calculator: Calculator
     var body: some View {
         GeometryReader {geometry in
             VStack(alignment: .trailing, spacing: 0){
                 Spacer()
                 //Display the current value
-                Text("2")
+                Text(calculator.displayValue)
                     .foregroundColor(.white)
                     .font(.system(size: 40))
                     .lineLimit(1)
@@ -43,5 +44,6 @@ struct CalculatorHome: View {
 struct CalculatorHome_Previews: PreviewProvider {
     static var previews: some View {
         CalculatorHome()
+            .environmentObject(Calculator())
     }
 }
